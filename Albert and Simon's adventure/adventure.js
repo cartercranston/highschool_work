dd1e2ebc78a660cc92457757140f8da21549a74c
mountdougstudent <77118760+mountdougstudent@users.noreply.github.com>
2021-01-26 20:35:38 -0800

    Final upload without photon torpedos

--- Albert and Simon's adventure/adventure.js
+++ Albert and Simon's adventure/adventure.js
@@ -1,6 +1,9 @@
-/*TODO
- * Photon torpedos
- * Home Screen
+/*
+TODO
+ * Charging animation
+*/
+/*Note:
+Whenever adding new obstacle types, animateTorpedo must be changed
 */
 const levels = [
 	//level 0
@@ -40,7 +43,7 @@ const levels = [
 	 "","obstacle1","","track1","track1","track1","track1","","",""],
 	 
 	 //level 3
-	["","","","","","","","","","",
+	["","obstacle1","obstacle1","obstacle1","obstacle1","obstacle1","","","","",
 	 "track1","track1","track1","track1","","","","","","track3",
      "","obstacle1","","forcefieldup","obstacle1","","forcefieldup","track2","","track3",
 	 "","obstacle1","orbit","obstacle1","","","forcefieldup","track2","","track3",
@@ -52,30 +55,31 @@ const levels = [
 	 "","","","","flagandsphere","","","","",""]
  ];
  
-const gridBoxes = document.querySelectorAll("#gameboard div");
-const width = 10;
-const noPassObstacles = ["obstacle1","forcefieldup","forcefieldside","planet","planet2","planet3","planet4"];
+const gridBoxes = document.querySelectorAll("#gameboard div");//an array of div tags, from left to right, top to bottom
+const width = 10;//number of div tags per row and column
+const noPassObstacles = ["obstacle1","forcefieldup","forcefieldside","planet","planet2","planet3","planet4"];//classes that can't be moved through
 var currentLevel = 0;//starting level
 var currentLocationOfSphere = 0;
-var animations = [];//allow 3 enemies per level
-var torpedos = [];//allows each ship to have a photon torpedo
-var fireTorpedos = [];//allows each ship to fire repeatedly
-var torpedoPositions = [];//stores all torpedo positions
-var torpedoExists = [false,false,false];
-var i;
-var flagLocation = 0;
-var planetLocation = 0;
-var planetAssimilated = false;
-var gameWinnable = false;
+var animations = [];//allow 3 enemies per level by storing setTimeouts
+var torpedos = [];//allows each ship to have a photon torpedo by storing setTimeouts
+var fireTorpedos = [];//allows each ship to fire repeatedly by storing setTimeouts
+var torpedoPositions = [];//stores all torpedo positions as indexes for gridBoxes
+var torpedoExists = [false,false,false];//for each torpedo, stores whether it exists
+var flagLocation = 0;//stores the location of the cube
+var planetLocation = 0;//stores the location of the planet for assimilation animation
+var planetAssimilated = false;//false, underway or true. Only when true can gameWinnable change
+var gameWinnable = false;//becomes true when you return to an assimilated planet, allows you to win by returning to cube
+var enemyIndexes = [];//stores the current location of each enemy as an index of its boxes array
+var enemyBoxes = [[],[],[]];//2d array storing the boxes for the track of each enemy
+var enemyBoxesPositions = [[],[],[]];//as enemyBoxes, but stores the position instead of the div tag itself
 
 //start game
-window.addEventListener("load", function () {
+window.addEventListener("load", function () { 
 	loadLevel();
 });
 
 //move sphere
 document.addEventListener("keydown",function(e) {
-	
 	switch(e.keyCode) {
 		case 37:
 			if (currentLocationOfSphere % width !== 0) {
@@ -101,12 +105,9 @@ document.addEventListener("keydown",function(e) {
 });//key event listener
 
 
-//loadLevels 0 - maxLevel
+//load levels 0 through maxLevel
 function loadLevel () {
 	let levelMap = levels[currentLevel];
-	let enemy0Boxes;
-	let enemy1Boxes;
-	let enemy3Boxes;
 	planetAssimilated = false;
     gameWinnable = false;
 	
@@ -121,15 +122,38 @@ function loadLevel () {
 		}//else if
 	}//for
     
-	enemy0Boxes = document.querySelectorAll(".track1");
-	enemy1Boxes = document.querySelectorAll(".track2");
-	enemy3Boxes = document.querySelectorAll(".track3");
-	animateEnemy(enemy0Boxes, 0, "right", 0);
-	animateEnemy(enemy1Boxes, 0, "down", 1);
-	animateEnemy(enemy3Boxes, 0, "down", 2);
-	fireTorpedo(6, "right", 0);
-	fireTorpedo(11, "down", 1);
-	fireTorpedo(25, "down", 2);
+	enemyBoxes[0] = document.querySelectorAll(".track1");
+	enemyBoxes[1] = document.querySelectorAll(".track2");
+	enemyBoxes[2] = document.querySelectorAll(".track3");
+    
+    for (i = 0; i < enemyBoxes[0].length; i ++) {
+        for(j = 0; j < gridBoxes.length; j ++) {
+            if(gridBoxes[j] == enemyBoxes[0][i]) {
+                enemyBoxesPositions[0][i] = j;
+            }//if
+        }//for
+    }//for
+    for (i = 0; i < enemyBoxes[1].length; i ++) {
+        for(j = 0; j < gridBoxes.length; j ++) {
+            if(gridBoxes[j] == enemyBoxes[1][i]) {
+                enemyBoxesPositions[1][i] = j;
+            }//if
+        }//for
+    }//for
+    for (i = 0; i < enemyBoxes[2].length; i ++) {
+        for(j = 0; j < gridBoxes.length; j ++) {
+            if(gridBoxes[j] == enemyBoxes[2][i]) {
+                enemyBoxesPositions[2][i] = j;
+            }//if
+        }//for
+    }//for
+        
+    animateEnemy(enemyBoxes[0], 0, "right", 0);
+	animateEnemy(enemyBoxes[1], 0, "down", 1);
+	animateEnemy(enemyBoxes[2], 0, "down", 2);
+	//chargeTorpedo(enemyBoxesPositions[0], "right", 0, "torpedo0");
+	//chargeTorpedo(enemyBoxesPositions[1], "down", 1, "torpedo1");
+	//chargeTorpedo(enemyBoxesPositions[2], "down", 2, "torpedo2");
 }//loadLevel
 
 //animate enemy left to right (could add up and down to this)
@@ -144,11 +168,18 @@ function animateEnemy(boxes,index,direction,animation) {
 		return;
 	}//if
 	
-	//if the enemy hits you
+	//if the enemy hits you or a torpedo
 	if(boxes[index].className.includes("sphere")) {
 		document.getElementById("lose").style.display = "block";
         return;
-	}//if
+	} else if (boxes[index].className.includes("torpedo")) {
+        for (i = 0; i < torpedoPositions.length; i ++) {
+            if(gridBoxes[torpedoPositions[i]] == boxes[index]) {
+                torpedoExists[i] = false;
+                torpedoPositions[i] = null;
+            }//if
+        }//for
+    }//else if
 	
 	//update images
 	if(direction == "right") {
@@ -215,17 +246,43 @@ function animateEnemy(boxes,index,direction,animation) {
 		}//else
 	}//else if
 	
+    enemyIndexes[animation] = index;
+    
 	animations[animation] = setTimeout(function() {
 		animateEnemy(boxes, index, direction, animation);
 	}, 750);
 }//animate enemy
 
-//gets called three times at the start of the program, and then every few seconds later. Only works if the source ship's torpedo has crashed or gone off the screen. Moves the torpedo to a space adjacent to the ship, and animate it perpendicular to the ship
-function fireTorpedo (position, shipDirection, torpedo) {
-	
-	//Only works if the source ship's torpedo has crashed or gone off the screen.
+//gets called three times at the start of the program
+/* 
+ * * charge for 2 seconds with animation
+ * * choose a direction perpendicular to ship direction
+ * * wait until torpedo can be fired in that direction
+ * * fire torpedo
+ * * wait 2 seconds and for torpedo to crash
+ * * repeat
+ */
+function chargeTorpedo (currentEnemyBoxesPositions, shipDirection, torpedo, image) {
+    
+	//charge for 2 seconds with animation
+    chargingAnimation(torpedo);//torpedo here refers to the ship that fired the torpedo
+    setTimeout(function(){
+    
+        //choose a direction perpendicular to ship direction
+        if (shipDirection == "up" || shipDirection == "down") {
+            direction = (Math.random() < 0.5) ? "left":"right";
+        } else {
+            direction = (Math.random() < 0.5) ? "up":"down";
+        }//else
+
+        //wait until torpedo can be fired in that direction
+        fireTorpedo(currentEnemyBoxesPositions, direction, torpedo, image); 
+    }, 2000);
+    
+    
+    //Only works if the source ship's torpedo has crashed or gone off the screen.
 	if(!torpedoExists[torpedo]) {
-		
+        
 		//Moves the torpedo to a space adjacent to the ship, and animates it perpendicular to the ship
 		if (shipDirection == "up" || shipDirection == "down") {
 			direction = (Math.random() < 0.5) ? "left":"right";
@@ -233,19 +290,20 @@ function fireTorpedo (position, shipDirection, torpedo) {
 			direction = (Math.random() < 0.5) ? "up":"down";
 		}//else
 		
-		//animateTorpedo(direction, position, torpedo)
 		torpedoExists[torpedo] = true;
+        animateTorpedo(currentEnemyBoxesPositions[enemyIndexes[torpedo]], direction, torpedo, image);
 		
 	}//if
-	//gets called three times at the start of the program, and then every few seconds later.
+    
+	//gets called every four seconds later.
 	fireTorpedos[torpedo] = setTimeout(function() {
-		fireTorpedo(torpedo);
+		chargeTorpedo(currentEnemyBoxesPositions, shipDirection, torpedo, image);
 	}, 4000);
-}//fireTorpedo
+}//chargeTorpedo
 
-//
-function animateTorpedo (direction, position, torpedo) {
-	
+//every few seconds, the torpedo moves
+function animateTorpedo (position, direction, torpedo, image) {    
+    
 	//when position is increased by speed, it can go in any of the four directions
 	let speed;
 	switch (direction) {
@@ -258,42 +316,87 @@ function animateTorpedo (direction, position, torpedo) {
 		case "down": speed = 10;
 		break;
 	}//switch
-		
-	console.log(position, gridBoxes[position]);
-		
+    
 	//if the torpedo hits you, you lose
 	if(gridBoxes[position].className.includes("sphere")) {
 		document.getElementById("lose").style.display = "block";
         return;
 	}//if
 	
-	//update image
-	gridBoxes[position].classList.add("torpedo");
-		
-	//remove other images
-	for(i = 0; i < gridBoxes.length; i ++) {
-		if(!torpedoPositions.includes(i)) {
-			gridBoxes[i].classList.remove("torpedo");
-		}//if
-	}//for*/
-	
 	//moving
 	position += speed;
 	
 	//check if torpedo has crashed
-	/*if(torpedo has crashed){
-		torpedoExists[torpedo] = false;
-	} else {
-		
+    if(  ( Math.floor((position - speed) / 10) != Math.floor(position / 10) && ( (direction == "right") || (direction == "left") ) )|| (((position - speed) % 10 != position % 10) && (direction == "up" || direction == "down")) || position > 99 || position < 0 || gridBoxes[position].className.includes(noPassObstacles[0]) || gridBoxes[position].className.includes(noPassObstacles[0]) || gridBoxes[position].className.includes(noPassObstacles[1]) || gridBoxes[position].className.includes(noPassObstacles[2]) || gridBoxes[position].className.includes(noPassObstacles[3]) || gridBoxes[position].className.includes(noPassObstacles[4]) || gridBoxes[position].className.includes(noPassObstacles[5]) || gridBoxes[position].className.includes(noPassObstacles[6]) || gridBoxes[position].className.includes("enemy") || gridBoxes[position].className.includes("flag")){
+        torpedoExists[torpedo] = false;
+        torpedoPositions[torpedo] = null;
+	} else if (gridBoxes[position].className.includes("sphere")) {
+        document.getElementById("lose").style.display = "block";
+        return;
+    } else {
+        
 		//tell other torpedos where this one is
 		torpedoPositions[torpedo] = position;
-	}//else*/
-	
-	//set timeout to animate again
-	torpedos[torpedo] = setTimeout(function() {
-		animateTorpedo(direction, position, torpedo);
-	}, 1000);
+        
+        //set timeout to animate again
+        torpedos[torpedo] = setTimeout(function() {
+            animateTorpedo(position, direction, torpedo, image);
+        }, 1000);
+	}//else
+    
+    //update image
+	if(torpedoExists[torpedo]) {
+        gridBoxes[position].classList.add(image);
+    }//if
+		
+	//remove other images
+	for(i = 0; i < gridBoxes.length; i ++) {
+		if(!torpedoPositions.includes(i)) {
+			gridBoxes[i].classList.remove(image);
+		}//if
+        else {
+        }
+	}//for
 }//animateTorpedo
+
+//finish the process started by chargeTorpedo
+function fireTorpedo (currentEnemyBoxesPositions, direction, torpedo, param4) {
+    let torpedoSpeed 
+    
+    switch (direction) {
+		case "right": torpedoSpeed = 1;
+		break;
+		case "left": torpedoSpeed = -1;
+		break;
+		case "up": torpedoSpeed = -10;
+		break;
+		case "down": torpedoSpeed = 10;
+		break;
+	}//switch
+    
+    //if torpedo will crash
+    if(gridBoxes[currentEnemyBoxesPositions[enemyIndexes[torpedo]] + torpedoSpeed].className.includes(noPassObstacles[0]) || gridBoxes[currentEnemyBoxesPositions[enemyIndexes[torpedo]] + torpedoSpeed].className.includes(noPassObstacles[1]) || gridBoxes[currentEnemyBoxesPositions[enemyIndexes[torpedo]] + torpedoSpeed].className.includes(noPassObstacles[2]) || gridBoxes[currentEnemyBoxesPositions[enemyIndexes[torpedo]] + torpedoSpeed].className.includes(noPassObstacles[3]) || gridBoxes[currentEnemyBoxesPositions[enemyIndexes[torpedo]] + torpedoSpeed].className.includes(noPassObstacles[4]) || gridBoxes[currentEnemyBoxesPositions[enemyIndexes[torpedo]] + torpedoSpeed].className.includes(noPassObstacles[5]) || gridBoxes[currentEnemyBoxesPositions[enemyIndexes[torpedo]] + torpedoSpeed].className.includes(noPassObstacles[6])) {
+        
+        //fire torpedo
+        torpedoExists[torpedo] = true;
+        animateTorpedo(currentEnemyBoxesPositions[enemyIndexes[torpedo]], direction, torpedo, param4);
+    
+        //wait 2 seconds and for torpedo to crash, then repeat
+        setTimeout(torpedoHasCrashed(currentEnemyBoxesPositions, direction, torpedo, param4), 2000);
+    } else {
+        //wait half a second, then try
+        setTimeout(fireTorpedo(currentEnemyBoxesPositions, direction, torpedo, param4), 3000);
+    }//else
+}//fireTorpedo
+
+//checks if torpedo has crashed. If it hasn't, wait half a second and call this again
+function torpedoHasCrashed (param1, param2, torpedo, param4) {
+    if(torpedoExists[torpedo]) {
+        setTimeout(torpedoHasCrashed(param1, param2, torpedo, param4), 500);
+    } else {
+        chargeTorpedo(param1, param2, torpedo, param4);
+    }//else
+}//torpedoHasCrashed
 
 //try to move sphere
 function tryToMove (direction) {
@@ -325,7 +428,7 @@ function tryToMove (direction) {
     }//if
 	
 	//if the space is adjacent to a planet, start assimilation
-    if (nextClass == "orbit") {
+    if (nextClass.includes("orbit")) {
 		orbit();
     }//if
     
@@ -347,8 +450,8 @@ function tryToMove (direction) {
         newClass += " orbit";
     }//if
     
-    //if it is an enemy, you lose
-    if(nextClass.includes("enemy")){
+    //if it is an enemy or torpedo, you lose
+    if(nextClass.includes("enemy") || nextClass.includes("torpedo")){
         document.getElementById("lose").style.display = "block";
         return;
     }//if
@@ -369,19 +472,19 @@ function tryToMove (direction) {
 
 //move up a level when at flag
 function levelUp() {
-    document.getElementById("levelup").style.display = "block";
-    clearTimeout(animations[0]);
-	clearTimeout(animations[1]);
-	clearTimeout(animations[2]);
-    setTimeout(function(){
-        document.getElementById("levelup").style.display = "none";
-        currentLevel ++;
-        if (currentLevel < levels.length) {
+    if (currentLevel < levels.length) {
+        document.getElementById("levelup").style.display = "block";
+        clearTimeout(animations[0]);
+        clearTimeout(animations[1]);
+        clearTimeout(animations[2]);
+        setTimeout(function(){
+            document.getElementById("levelup").style.display = "none";
+            currentLevel ++;
             loadLevel();
-        } else {
-			document.getElementById("gamewon").style.display = "block";
-		}//else
-    }, 1000);//setTimeout
+        }, 1000);//setTimeout
+    } else {
+            window.location.href = "end.html";
+	}//else
 }//levelUp
 
 //when sphere is next to planet, can beam drones up or down
@@ -410,7 +513,7 @@ function assimilatePlanet () {
         planetAssimilated = true;
 		if(gridBoxes[currentLocationOfSphere].className.includes("orbit")) {
 			orbit();
-		}
+		}//if
     }, 4000);
 	
 	//make explosions for 4 seconds
@@ -430,4 +533,13 @@ function assimilatePlanet () {
 	}//for
 }//assimilatePlanet
 
-
+//changes the appearance of the ship in a few steps
+function chargingAnimation (ship) {
+    //convert ship to charge1
+    //document.getElementsByClassName("enemyup").style.backgroundImage = "url('images/charge1enemyup')";
+    //document.getElementsByClassName("enemydown").style.backgroundImage = "url('images/charge1enemydown')";
+    
+    setTimeout(function() {
+        //convert ship to charge2
+    }, 1000);
+}//chargingAnimation
